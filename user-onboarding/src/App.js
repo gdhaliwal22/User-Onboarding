e453eccb767ae068d1ce0c345a31209048c5c0aa
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-09-19 15:20:59 -0700

    create form.js component imported into App.js

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormikUserForm from "./components/Form"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <UserForm />
+    </div>
+  );
+}
+    
+
+export default App;
